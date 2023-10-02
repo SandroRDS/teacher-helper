50df761fe6d837d3edcdba8cd0ca3cb278324014
SandroRDS <sandror.desouza847@gmail.com>
2023-10-02 12:41:58 -0300

    Removendo PageImpress da página

--- src/components/PdfReportMaker/index.tsx
+++ src/components/PdfReportMaker/index.tsx
@@ -12,38 +12,12 @@ const Main = styled.main`
 `;
 
 function PdfReportMaker() {
-    const report: ReportType = {
-        studentName: 'Ana Carolina Oliveira Batista',
-        studentModule: 'CT1',
-        teacherObservation: 'A Ana é uma ótima aluna, muito carismática e inteligente.',
-        feedbacks: [
-            {
-                category: 'Atenção e Participação em sala',
-                feedback: 'Ótimo',
-                observation: 'Tem uma ótima participação de aula, respeita e interage muito bem com os alunos e professores.'
-            },
-            {
-                category: 'Atenção e Participação em sala',
-                feedback: 'Ótimo',
-                observation: 'Tem uma ótima participação de aula, respeita e interage muito bem com os alunos e professores.'
-            },
-            {
-                category: 'Atenção e Participação em sala',
-                feedback: 'Ótimo',
-                observation: 'Tem uma ótima participação de aula, respeita e interage muito bem com os alunos e professores.'
-            }
-        ],
-        courseProgramName: 'Control+Kids 1 Online',
-        courseProgramTools: ['Scratch', 'Scratch', 'Scratch', 'Scratch', 'Scratch', 'Scratch', 'Scratch'],
-    };
-    
     return (
         <>
             <GoBack />
             <Main>
                 <p>PdfReportMaker Page</p>
             </Main>
-            <PageImpress {...report}/>
         </>
     );
 }
